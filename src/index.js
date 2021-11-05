44b12fa89dc2315bb04db03f04acbfc3e06c7040
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-05 10:16:12 -0300

    add rotas para usuario

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 require('dotenv').config()
 
+const { userRouter } = require('./routes');
+
 const app = express();
 
 app.get('/ping', (_req, res) => res.status(200).json({message: 'pong'}))
+
+app.use('/user', userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
---
+++ src/routes/userRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const userController = require('../controllers');
+
+const route = express.Router();
+
+route.post('/create', userController.create);
+
+route.post('/login', userController.login);
+
+route.post('/:id', userController.buy);
+
+module.exports = route;
